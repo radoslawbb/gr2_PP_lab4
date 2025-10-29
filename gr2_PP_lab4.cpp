d71d758ab9918c39ab417aca1d39fb85b35d8a30
radoslawbb <radbal2005@gmail.com>
2025-10-29 12:56:30 +0100

    lab4

--- gr2_PP_lab4.cpp
+++ gr2_PP_lab4.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 using namespace std;
 
+
+/*zadanie 1
 void funkcja1(int a, int b) {
     if (a > b) {
         cout << "Liczba " << a << " jest wieksza" << endl;
@@ -20,10 +22,45 @@ int funkcja2(int a, int b) {
     else {
         return b;
     }
+}*/
+
+/*zadanie 2
+void dzielniki(int n) {
+    cout << "Dzielniki liczby " << n << ":" << endl;
+    for (int i = 1; i <= n; i++) {
+        if (n % i == 0) {
+            cout << i << endl;
+        }
+    }
+}*/
+
+
+/*zadanie 3
+float dodaj(float x, float y) {
+    return x + y;
 }
+
+float odejmij(float x, float y) {
+    return x - y;
+}
+
+float mnoz(float x, float y) {
+    return x * y;
+}
+
+float dziel(float x, float y) {
+    if (x != 0 && y != 0) {
+        cout << x << " / " << y << " = " << x/y << endl;
+        return x / y;
+    }
+    else {
+        cout << "nie mozna dzielic przez 0" << endl;
+    }
+}*/
 
 int main()
 {
+    /*zadanie 1
     int x, y;
     cout << "Wprowadz liczbe a: ";
     cin >> x;
@@ -31,7 +68,64 @@ int main()
     cin >> y;
 
     funkcja1(x, y);
-    cout << "Wieksza liczba to: " << funkcja2(x, y) << endl;
+    cout << "Wieksza liczba to: " << funkcja2(x, y) << endl;*/
+
+    /*zadanie 2
+    int x;
+    do {
+        cout << "Wprowadz liczbe calkowita dodatnia: ";
+        cin >> x;
+    } while (x < 1);
+    
+    if (x >= 1) {
+        dzielniki(x);
+    }*/
+
+
+    /*zadanie 3
+    int koniec = 1;
+    while (koniec) {
+        float x, y, wyn;
+        char znak;
+        cout << "\nPodaj liczbe x: ";
+        cin >> x;
+        cout << "\nPodaj liczbe y: ";
+        cin >> y;
+
+        cout << "\nPodaj znak dla pozadanego dzialania: ";
+        cin >> znak;
+
+        cout << endl;
+
+        switch (znak) {
+        case '+':
+            cout << x << " + " << y << " = " << dodaj(x,y) << endl;
+            break;
+        case '-':
+            cout << x << " - " << y << " = " << odejmij(x,y) << endl;
+            break;
+        case '*':
+            cout << x << " * " << y << " = " << mnoz(x,y) << endl;
+            break;
+        case '/':
+            dziel(x, y);
+            break;
+        default:
+            cout << "Wprowadzono niepoprawny znak" << endl;
+            break;
+        }
+
+        cout << "Zakonczyc program? Wpisz 0 zeby zakonczyc: ";
+        cin >> koniec;
+
+        if (koniec == 0) {
+            return 0;
+        }
+    }*/
+
+
+
+
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
